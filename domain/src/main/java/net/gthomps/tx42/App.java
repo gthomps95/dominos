739b88959c7097e6b8f4f241b6b63bccc13caca0
Greg Thompson <greg_d_thompson@yahoo.com>
2011-06-26 16:36:19 -0500

    initial project version

---
+++ domain/src/main/java/net/gthomps/tx42/App.java
@@ -0,0 +1,13 @@
+package net.gthomps.tx42;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
